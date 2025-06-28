abf2391f6c9ff1e2a52f7c62feebb11ef87a5f53
Dilvi <110786398+Dilvi@users.noreply.github.com>
2025-06-28 23:45:13 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	const USD = 78.47
-	const EUR = 92.27
-	EUR_value := 1.0
-	EUR_to_USD := USD / EUR
-	EUR_in_RUB := EUR_value * EUR_to_USD * USD
-	fmt.Print(EUR_in_RUB)
+	const USD_TO_EUR = 0.85     
+	const USD_TO_RUB = 78.47   
+	EURValue := 1.0
+	EUR_to_RUB := EURValue * (USD_TO_RUB / USD_TO_EUR)
+	fmt.Print(EUR_to_RUB)
 }
