1b13ec6f1e27fea43ae4e98e6ac10984ba1616ad
Dilvi <110786398+Dilvi@users.noreply.github.com>
2025-07-03 22:44:46 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	const USD = 78.47
-	const EUR = 92.28
-	EUR_value := 1.0
-	EUR_to_USD := USD / EUR
-	EUR_in_RUB := EUR_value * EUR_to_USD * USD
-	fmt.Print(EUR_in_RUB)
+	const USD_TO_EUR = 0.85     
+	const USD_TO_RUB = 78.47   
+	EURValue := userInput()
+	EUR_to_RUB := EURValue * (USD_TO_RUB / USD_TO_EUR)
+	fmt.Print(EUR_to_RUB)
+}
+
+func userInput() float64 {
+	var EURValue float64
+	fmt.Println("Введите количество Евро, которые вы хотите перевести в рубли")
+	fmt.Scan(&EURValue) 
+	return EURValue
+}
+
+
+func currencyConverter(amount float64, from string, to string) float64 {
+	var result float64
+	return result
 }
