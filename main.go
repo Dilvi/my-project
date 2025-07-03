79eb7acd5ace7719a96eb4e41e7b8eb0d759f6cf
Dilvi <artem04032004@yandex.ru>
2025-07-03 22:32:47 +0300

    Начальный коммит проекта в ветку 1-start

--- main.go
+++ main.go
@@ -5,7 +5,20 @@ import "fmt"
 func main() {
 	const USD_TO_EUR = 0.85     
 	const USD_TO_RUB = 78.47   
-	EURValue := 1.0
+	EURValue := userInput()
 	EUR_to_RUB := EURValue * (USD_TO_RUB / USD_TO_EUR)
 	fmt.Print(EUR_to_RUB)
+}
+
+func userInput() float64 {
+	var EURValue float64
+	fmt.Println("Введите количество Евро, которые вы хотите перевести в рубли")
+	fmt.Scan(EURValue)
+	return EURValue
+}
+
+// Пустая функция без реализации
+func currencyConverter(a,b,c float64) float64 { // Принимает 3 значения: 1 число и 2 валюты - исходная и целевая
+	var d float64 // Выходное число
+	return d
 }
