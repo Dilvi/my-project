8109255db1e101974092bffc06299f99dfb1f1a2
Dilvi <110786398+Dilvi@users.noreply.github.com>
2025-07-07 19:05:47 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,22 +3,78 @@ package main
 import "fmt"
 
 func main() {
-	const USD_TO_EUR = 0.85     
-	const USD_TO_RUB = 78.47   
-	EURValue := userInput()
-	EUR_to_RUB := EURValue * (USD_TO_RUB / USD_TO_EUR)
-	fmt.Print(EUR_to_RUB)
+	fmt.Println("Конвертер валюты")
+	currencyInput, currencyValue, currencyOutput := userInput()
+	result := currencyConverter(currencyInput, currencyValue, currencyOutput)
+	fmt.Printf("Результат: %.2f", result)
 }
 
-func userInput() float64 {
-	var EURValue float64
-	fmt.Println("Введите количество Евро, которые вы хотите перевести в рубли")
-	fmt.Scan(&EURValue) 
-	return EURValue
+func userInput() (int, float64, int) {
+	var currencyInput int
+	var currencyValue float64
+	var currencyOutput int
+	for {
+		fmt.Println("Введите конвертируемую валюту")
+		fmt.Println("1 - EUR")
+		fmt.Println("2 - USD")
+		fmt.Println("3 - RUB")
+		fmt.Scan(&currencyInput)
+		if currencyInput != 1 && currencyInput != 2 && currencyInput != 3 {
+			fmt.Println("Ошибка при вводе, повторите ввод")
+		} else {
+			break
+		}
+	}
+	for {
+		fmt.Println("Введите конвертируемую сумму")
+		fmt.Scan(&currencyValue)
+		if currencyValue <= 0 {
+			fmt.Println("Ошибка при вводе, повторите ввод")
+		} else {
+			break
+		}
+	}
+	for {
+		fmt.Println("В какую валюту вы хотите конвертировать?")
+		switch {
+		case currencyInput == 1:
+			fmt.Println("1 - USD")
+			fmt.Println("2 - RUB")
+		case currencyInput == 2:
+			fmt.Println("1 - EUR")
+			fmt.Println("2 - RUB")
+		case currencyInput == 3:
+			fmt.Println("1 - EUR")
+			fmt.Println("2 - USD")
+		}
+		fmt.Scan(&currencyOutput)
+		if currencyOutput != 1 && currencyOutput != 2{
+			fmt.Println("Ошибка при вводе, повторите ввод")
+		} else {
+			break
+		}
+	}
+	return currencyInput, currencyValue, currencyOutput
 }
 
 
-func currencyConverter(amount float64, from string, to string) float64 {
+func currencyConverter(currencyInput int, currencyValue float64, currencyOutput int) float64 {
 	var result float64
+	const USD_TO_EUR = 0.85     
+	const USD_TO_RUB = 78.47  
+	switch {
+	case currencyInput == 1, currencyOutput == 1:
+		result = currencyValue * (1 / USD_TO_EUR)
+	case currencyInput == 1, currencyOutput == 2:
+		result = currencyValue * (USD_TO_RUB / USD_TO_EUR)
+	case currencyInput == 2, currencyOutput == 1:
+		result = currencyValue * USD_TO_EUR
+	case currencyInput == 2, currencyOutput == 2:
+		result = currencyValue * USD_TO_RUB
+	case currencyInput == 3, currencyOutput == 1:
+		result = currencyValue * (USD_TO_EUR / USD_TO_RUB)
+	case currencyInput == 3, currencyOutput == 2:
+		result = currencyValue * (1 / USD_TO_RUB)
+	}
 	return result
 }
